a7d3a30367a1209088c9311d5d321ce557451976
KwangHee Cho <bcrisis29@gmail.com>
2024-08-25 14:37:27 +0900

    fake data 모델, 서비스 생성

--- src/main.py
+++ src/main.py
@@ -1,18 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
-from .web import explorer
+from .web import creature, explorer
 
 app = FastAPI()
 
 app.include_router(explorer.router)
+app.include_router(creature.router)
 
-@app.get("/")
-def top():
-    return "top here"
+# @app.get("/")
+# def top():
+#     return "top here"
 
-@app.get("/echo/{thing}")
-def echo(thing):
-    return f"echoing {thing}"
+# @app.get("/echo/{thing}")
+# def echo(thing):
+#     return f"echoing {thing}"
 
 if __name__ == '__main__':
     
